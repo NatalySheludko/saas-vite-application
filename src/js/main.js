a99e62f77193595191aaf9f234a9b59e970de185
Nataliia Sheludko <sheludkonataliyavaleriivna@gmail.com>
2024-12-02 23:30:22 +0200

    add toggle for sidebar items

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,7 @@ import '../scss/main.scss';
 
 import * as bodyScrollLock from 'body-scroll-lock';
 
+
 // Sidebar menu
 const refsMenu = {
   openMenuBtn: document.querySelector('.js-menu-open'),
@@ -25,6 +26,7 @@ const toggleMenu = () => {
 refsMenu.openMenuBtn.addEventListener('click', toggleMenu);
 refsMenu.closeMenuBtn.addEventListener('click', toggleMenu);
 
+
 // Close the mobile menu on wider screens if the device orientation changes
 window.matchMedia('(min-width: 1200px)').addEventListener('change', event => {
   if (!event.matches) return;
@@ -32,4 +34,71 @@ window.matchMedia('(min-width: 1200px)').addEventListener('change', event => {
   refsMenu.overlayMenu.classList.remove('is-open');
   refsMenu.openMenuBtn.setAttribute('aria-expanded', false);
   bodyScrollLock.enableBodyScroll(document.body);
+});
+
+
+// Sidebar toggle
+const elements = {
+  menuLinks: document.querySelectorAll('.menu-link'),
+  overviewSections: document.querySelectorAll('.overview'),
+  companiesSection: document.querySelector('.companies'),
+  overviewsSidebarItem: document.querySelector('.sidebar-overview'),
+  companiesSidebarItem: document.querySelector('.sidebar-companies'),
+  mainElement: document.querySelector('.main'),
+};
+
+function removeCurrentClass() {
+  elements.menuLinks.forEach(link => {
+    link.classList.remove('current');
+  });
+}
+
+function setActiveLink(link) {
+  removeCurrentClass();
+  link.classList.add('current');
+}
+
+function showOverview() {
+  if (elements.companiesSection) {
+    elements.companiesSection.classList.add('visually-hidden');
+  }
+
+  elements.overviewSections.forEach(section => {
+    section.classList.remove('visually-hidden');
+  });
+
+  if (elements.mainElement) {
+    elements.mainElement.style.display = 'grid';
+  }
+}
+
+function showCompanies() {
+  elements.overviewSections.forEach(section => {
+    section.classList.add('visually-hidden');
+  });
+
+  if (elements.companiesSection) {
+    elements.companiesSection.classList.remove('visually-hidden');
+  }
+
+  if (elements.mainElement) {
+    elements.mainElement.style.display = 'block';
+  }
+}
+
+elements.overviewsSidebarItem.addEventListener('click', event => {
+  event.preventDefault();
+  showOverview();
+  setActiveLink(event.currentTarget.querySelector('.menu-link'));
+});
+
+elements.companiesSidebarItem.addEventListener('click', event => {
+  event.preventDefault();
+  showCompanies();
+  setActiveLink(event.currentTarget.querySelector('.menu-link'));
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+  showOverview();
+  setActiveLink(elements.overviewsSidebarItem.querySelector('.menu-link'));
 });
