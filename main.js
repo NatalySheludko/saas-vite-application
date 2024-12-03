88ec15aad07d987f5ac37011d884c55b087622e8
Nataliia Sheludko <sheludkonataliyavaleriivna@gmail.com>
2024-12-03 17:18:50 +0200

    update path, replace files

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // Commect styles
-import '../scss/main.scss';
+import './src/scss/style.scss';
 
 import * as bodyScrollLock from 'body-scroll-lock';
 
--- vite.config.js
+++ vite.config.js
@@ -1,51 +1,26 @@
-import imagemin from 'imagemin';
-import imageminWebp from 'imagemin-webp';
-import path from 'path';
 import { defineConfig } from 'vite';
-import glob from 'fast-glob';
-import { fileURLToPath } from 'url';
-import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
+import viteImagemin from 'vite-plugin-imagemin';
+import path from 'path';
 
 export default defineConfig({
-  base: '/sass-vite-application/',
   plugins: [
-    ViteImageOptimizer({
-      png: {
-        quality: 86,
-      },
-      jpeg: {
-        quality: 86,
-      },
-      jpg: {
-        quality: 86,
-      },
+    viteImagemin({
+      gifsicle: { optimizationLevel: 7 },
+      pngquant: { quality: [0.8, 0.9] },
+      mozjpeg: { quality: 86 },
+      webp: { quality: 86 },
     }),
-    {
-      ...imagemin(['./src/img/**/*.{jpg,png,jpeg}'], {
-        destination: './src/img/webp/',
-        plugins: [imageminWebp({ quality: 86 })],
-      }),
-      apply: 'serve',
-    },
   ],
   build: {
-    outDir: 'dist',
-    minify: false, // disable minification
     rollupOptions: {
-      input: Object.fromEntries(
-        glob
-          .sync(['./*.html', './pages/**/*.html'])
-          .map(file => [
-            path.relative(
-              __dirname,
-              file.slice(0, file.length - path.extname(file).length)
-            ),
-            fileURLToPath(new URL(file, import.meta.url)),
-          ])
-      ),
-      // output unminified CSS file
+      input: {
+        main: path.resolve(__dirname, 'index.html'),
+      },
       output: {
-        assetFileNames: 'assets/[name].[ext]',
+        assetFileNames: assetInfo => {
+          if (assetInfo.name === 'style.css') return 'css/style.css';
+          return 'assets/[name].[ext]';
+        },
       },
     },
   },
